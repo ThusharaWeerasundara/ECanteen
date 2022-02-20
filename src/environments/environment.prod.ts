1d83d799f8c0632b792791385c2b811ca2029a6d
thushara weerasundara <e16388@eng.pdn.ac.lk>
2022-02-21 01:07:34 +0530

    Project initialized

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'canteen-dfadd',
+    appId: '1:1044677760536:web:25914b16bccecccd23d56d',
+    storageBucket: 'canteen-dfadd.appspot.com',
+    apiKey: 'AIzaSyD6tVfXwLz8Ic6vkHhM8nrxbzqBKRwaPrs',
+    authDomain: 'canteen-dfadd.firebaseapp.com',
+    messagingSenderId: '1044677760536',
+    measurementId: 'G-Q50PGR7K9K',
+  },
+  production: true
+};
